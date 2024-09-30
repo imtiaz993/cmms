f88544192d56733f57a22d98edbeb2187c4134d9
imtiaz993 <imtiazh993@gmail.com>
2024-09-30 19:14:57 +0500

    antD with nexthjs

--- app/rootWrapper.js
+++ app/rootWrapper.js
@@ -1,24 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function RootWrapper({ children }) {
-  const [isWindow, setIsWindow] = useState();
-
-  useEffect(() => {
-    if (typeof window != "undefined") {
-      setIsWindow(true);
-    }
-  }, []);
-
   return (
-    isWindow && (
-      <div>
-        {children}
-        <ToastContainer />
-      </div>
-    )
+    <div>
+      <AntdRegistry>{children}</AntdRegistry>
+      <ToastContainer />
+    </div>
   );
 }
