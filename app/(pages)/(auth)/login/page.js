c435f8de24b207fe34a5068e76fa79e1e53e7aa4
Imtiaz Hussain <imtiazh993@gmail.com>
2024-10-01 22:01:02 +0500

    added reusable components & authentication

--- app/(pages)/(auth)/login/page.js
+++ app/(pages)/(auth)/login/page.js
@@ -1,85 +1,93 @@
 "use client";
-import { Space, Table, Tag } from "antd";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import { Input } from "antd";
+import {
+  EyeInvisibleOutlined,
+  EyeOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
+import Button from "@/components/common/Button";
+import InputField from "@/components/common/InputField";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <>
-      <h1 className="font-bold text-blue-500 text-9xl">Hello World</h1>
-      <Table columns={columns} dataSource={data} />
-    </>
+    <div className="flex flex-col justify-center items-center text-white min-h-screen w-11/12 mx-auto md:w-full max-w-[520px]">
+      <h1 className="text-2xl md:text-3xl font-bold">Login</h1>
+      <p className="mt-3 text-sm md:text-base">
+        Welcome back! Please enter your details
+      </p>
+      <div className="mt-10 w-full">
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values);
+            // setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting, handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              <InputField
+                placeholder="Email"
+                name="email"
+                as={Input}
+                prefix={<MailOutlined style={{ fontSize: "125%" }} />}
+              />
+              <InputField
+                name="password"
+                placeholder="Password"
+                type={showPassword ? "text" : "password"}
+                as={Input}
+                prefix={
+                  showPassword ? (
+                    <EyeOutlined
+                      style={{ fontSize: "125%" }}
+                      onClick={() => {
+                        setShowPassword((prev) => !prev);
+                      }}
+                    />
+                  ) : (
+                    <EyeInvisibleOutlined
+                      style={{ fontSize: "125%" }}
+                      onClick={() => {
+                        setShowPassword((prev) => !prev);
+                      }}
+                    />
+                  )
+                }
+                className="mt-7"
+              />
+              <Link
+                href="/forgot-password"
+                className="mt-4 text-sm font-medium text-white flex justify-end"
+              >
+                Forgot Password?
+              </Link>
+              <Button
+                text="Login Now"
+                htmlType="submit"
+                disabled={isSubmitting}
+                isLoading={isSubmitting}
+                className="mt-7"
+              />
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
 export default Login;
-
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
