cc0ce878c4769cc4918cbbe5f3e9575b9af906f3
imtiaz993 <imtiazh993@gmail.com>
2024-10-02 11:46:41 +0500

    side bar fixes

--- app/(pages)/admin/components/sidebar.jsx
+++ app/(pages)/admin/components/sidebar.jsx
@@ -1,37 +1,27 @@
-import { DownOutlined } from "@ant-design/icons";
 import Image from "next/image";
+import { DownOutlined } from "@ant-design/icons";
 import { sidebarOptions } from "./sidebarOptions";
-import { useState } from "react";
 
 const Sidebar = () => {
-  const [collapsed, setCollapsed] = useState(false);
   return (
     <div className="bg-primary min-h-[calc(100dvh-16px-60px)] hidden lg:block lg:w-[300px] rounded-tr-xl p-5  select-none">
-      <div
-        className=" text-xl mb-3"
-        onClick={() => setCollapsed(!collapsed)}
-      >
-        Location
-        <DownOutlined
-          className="ml-6 !text-secondary"
-          rotate={collapsed ? 180 : 0}
-        />
-      </div>
+      <div className=" text-lg mb-3">Locations</div>
       <div className="ml-3">
-        {collapsed &&
-          sidebarOptions.map((option, index) => (
-            <div key={index} className="mt-3 flex gap-9">
+        {sidebarOptions.map((option, index) => (
+          <div key={index} className="mt-3 flex gap-9">
+            <div className="flex items-center">
               <Image
                 src="/images/buildings.png"
                 alt="Buildings"
                 width={100}
                 height={100}
-                className="w-7 h-7"
+                className="w-5 h-5 mr-2"
               />
-              <p>{option.title}</p>
-              <DownOutlined className="!text-secondary" />
+              <p className="text-sm">{option.title}</p>
             </div>
-          ))}
+            <DownOutlined className="!text-secondary cursor-pointer" />
+          </div>
+        ))}
       </div>
     </div>
   );
