1bf6c442125ab445ae1caf0e06ac68fac393bc29
i-akbar <67053767+i-akbar@users.noreply.github.com>
2024-10-06 15:40:22 +0500

    Schedule page

--- app/(pages)/admin/dashboard/components/schedule/components/dailyBatchPopup.jsx
+++ app/(pages)/admin/dashboard/components/schedule/components/dailyBatchPopup.jsx
@@ -4,21 +4,38 @@ import Low from "./low";
 import Medium from "./medium";
 import High from "./high";
 import Critical from "./critical";
+import Button from "@/components/common/Button";
+import { Form, Formik } from "formik";
 
 const DailyBatchPopup = ({ selectedDate, setSelectedDate }) => {
   const [selectedTab, setSelectedTab] = useState("Critical"); // State to track the selected tab
+  const [batchEdit, setBatchEdit] = useState(false);
+  const [print, setPrint] = useState(false);
+  const [selectedItems, setSelectedItems] = useState(new Set());
+
+  const handleCheckboxChange = (item) => {
+    setSelectedItems((prev) => {
+      const newSelected = new Set(prev);
+      if (newSelected.has(item)) {
+        newSelected.delete(item);
+      } else {
+        newSelected.add(item);
+      }
+      return newSelected;
+    });
+  };
 
   // Function to render the content based on the selected radio button
   const renderContent = () => {
     switch (selectedTab) {
       case "Critical":
-        return <Critical />;
+        return <Critical batchEdit={batchEdit} print={print} />;
       case "High":
-        return <High />;
+        return <High batchEdit={batchEdit} print={print} />;
       case "Medium":
-        return <Medium />;
+        return <Medium batchEdit={batchEdit} print={print} />;
       case "Low":
-        return <Low />;
+        return <Low batchEdit={batchEdit} print={print} />;
       default:
         return null;
     }
@@ -26,41 +43,84 @@ const DailyBatchPopup = ({ selectedDate, setSelectedDate }) => {
 
   return (
     <div>
-      <Modal
-        maskClosable={false}
-        open={selectedDate}
-        onCancel={() => setSelectedDate("")}
-        footer={null}
-        title={"Daily Work Orders: " + selectedDate}
-        width={1000}
-      >
-        <div className="pb-4 pt-3">
-          <Radio.Group
-            onChange={(e) => setSelectedTab(e.target.value)}
-            value={selectedTab}
-            style={{
-              marginBottom: 16,
-              textAlign: "center",
-            }}
-          >
-            <Radio.Button value="Critical" style={{ width: "100px" }}>
-              Critical
-            </Radio.Button>
-            <Radio.Button value="High" style={{ width: "100px" }}>
-              High
-            </Radio.Button>
-            <Radio.Button value="Medium" style={{ width: "100px" }}>
-              Medium
-            </Radio.Button>
-            <Radio.Button value="Low" style={{ width: "100px" }}>
-              Low
-            </Radio.Button>
-          </Radio.Group>
-
-          {/* Render content based on selected radio button */}
-          <div>{renderContent()}</div>
-        </div>
-      </Modal>
+      <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
+        {({ values, setFieldValue }) => (
+          <Form>
+            <Modal
+              maskClosable={false}
+              open={selectedDate}
+              onCancel={() => setSelectedDate("")}
+              footer={null}
+              title={"Daily Work Orders: " + selectedDate}
+              width={1000}
+            >
+              <div className="pb-4 pt-3">
+                <div className="md:flex justify-between">
+                  <Radio.Group
+                    onChange={(e) => setSelectedTab(e.target.value)}
+                    value={selectedTab}
+                    style={{
+                      marginBottom: 16,
+                      textAlign: "center",
+                      display: "flex",
+                    }}
+                  >
+                    <Radio.Button value="Critical" className="w-full md:w-24">
+                      Critical
+                    </Radio.Button>
+                    <Radio.Button value="High" className="w-full md:w-24">
+                      High
+                    </Radio.Button>
+                    <Radio.Button value="Medium" className="w-full md:w-24">
+                      Medium
+                    </Radio.Button>
+                    <Radio.Button value="Low" className="w-full md:w-24">
+                      Low
+                    </Radio.Button>
+                  </Radio.Group>
+                  <div className="mb-2 md:mb-0 text-end">
+                    <Button
+                      outlined
+                      size="small"
+                      text="Print"
+                      style={{ width: "fit-content" }}
+                      className="mr-2"
+                      onClick={() => {
+                        setBatchEdit(false);
+                        setPrint(!print);
+                      }}
+                    />
+                    <Button
+                      outlined
+                      size="small"
+                      text="Batch Edit"
+                      style={{ width: "fit-content" }}
+                      className="mr-2"
+                      onClick={() => {
+                        setPrint(false);
+                        setBatchEdit(!batchEdit);
+                      }}
+                    />
+                  </div>
+                </div>
+                {/* Render selected items and button */}
+                {(batchEdit || print) && (
+                  <div className="flex justify-between items-center my-4">
+                    <div>{selectedItems.size} selected</div>
+                    <Button
+                      text={batchEdit ? "Reschedule" : print && "Print"}
+                      fullWidth={false}
+                      style={{ padding: "4px 10px" }}
+                    />
+                  </div>
+                )}
+                {/* Render content based on selected radio button */}
+                <div>{renderContent()}</div>
+              </div>
+            </Modal>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
--- app/(pages)/admin/dashboard/components/schedule/components/low.jsx
+++ app/(pages)/admin/dashboard/components/schedule/components/low.jsx
@@ -1,20 +1,18 @@
 import { WarningFilled } from "@ant-design/icons";
-import { Card, Typography } from "antd";
-const High = () => {
+import { Card, Checkbox, Typography } from "antd";
+const High = ({ batchEdit }) => {
   const { Text } = Typography;
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {Array.from({ length: 9 }).map((key, index) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {Array.from({ length: 10 }).map((key, index) => (
         <Card
           size="small"
           title={"11M1948377"}
-          style={{
-            width: 300,
-          }}
           headStyle={{
             backgroundColor: "#96b2b3",
-          }} // Adjust padding here
+          }}
+          extra={batchEdit && <Checkbox />}
           key={key}
         >
           <Text
