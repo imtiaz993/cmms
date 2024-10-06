23eaa1af5c60339b3d50aa1952c6820727db35a9
Imtiaz Hussain <imtiazh993@gmail.com>
2024-10-06 14:41:46 +0500

    completed charts in dashboard

--- app/(pages)/admin/dashboard/components/dashboard/index.jsx
+++ app/(pages)/admin/dashboard/components/dashboard/index.jsx
@@ -149,10 +149,10 @@ const ColumnChart = () => (
       },
       responsive: [
         {
-          breakpoint: 1400,
+          breakpoint: 1600,
           options: {
             chart: {
-              width: 400,
+              width: 370,
             },
             plotOptions: {
               bar: {
@@ -165,10 +165,18 @@ const ColumnChart = () => (
           },
         },
         {
-          breakpoint: 1150,
+          breakpoint: 1500,
           options: {
             chart: {
-              width: 320,
+              width: 310,
+            },
+          },
+        },
+        {
+          breakpoint: 1280,
+          options: {
+            chart: {
+              width: 500,
             },
           },
         },
@@ -194,108 +202,227 @@ const ColumnChart = () => (
       ],
     }}
     type="bar"
-    width={500}
+    width={400}
+    height={350}
+  />
+);
+
+const BarChart = () => (
+  <ReactApexChart
+    series={[
+      {
+        data: [400, 430, 448, 470, 540, 580, 690, 1100],
+      },
+    ]}
+    options={{
+      chart: {
+        type: "bar",
+        height: 350,
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: ["#40E0D0"],
+      grid: {
+        show: false,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          borderRadiusApplication: "end",
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        labels: {
+          style: {
+            colors: "#FFF",
+          },
+        },
+        categories: [
+          "Motorman",
+          "Floorman",
+          "MUD PUMP INSPECTOR",
+          "Driller",
+          "Rig Manager",
+          "Tool Pusher",
+          "Derrickman",
+          "Third Party",
+        ],
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: "#FFF",
+          },
+        },
+      },
+      tooltip: {
+        theme: "dark",
+      },
+      responsive: [
+        {
+          breakpoint: 1600,
+          options: {
+            chart: {
+              width: 370,
+            },
+            plotOptions: {
+              bar: {
+                columnWidth: "60%",
+              },
+            },
+            stroke: {
+              width: 5,
+            },
+          },
+        },
+        {
+          breakpoint: 1500,
+          options: {
+            chart: {
+              width: 310,
+            },
+          },
+        },
+        {
+          breakpoint: 1280,
+          options: {
+            chart: {
+              width: 500,
+            },
+          },
+        },
+        {
+          breakpoint: 768,
+          options: {
+            chart: {
+              width: 370,
+            },
+            legend: {
+              fontSize: "12px",
+            },
+          },
+        },
+        {
+          breakpoint: 430,
+          options: {
+            chart: {
+              width: 300,
+            },
+          },
+        },
+      ],
+    }}
+    type="bar"
+    width={400}
     height={350}
   />
 );
 
 const Dashboard = ({ activeLocation }) => {
   return (
-    <div className="grid md:grid-cols-2 gap-6 h-[calc(100dvh-140px)] overflow-auto px-3 lg:px-8 pb-4 pt-3">
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <p className="text-sm md:text-base">
-            Unplanned Work Orders{" "}
-            <sup className="text-xs font-normal">(Last 30 days)</sup>
-          </p>
-        }
-        extra={
-          <Link
-            href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
-            className="cursor-pointer text-secondary text-xs md:text-sm"
-          >
-            View All
-          </Link>
-        }
-      >
-        <div className="flex justify-center">
-          <DonutChart />
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <p className="text-sm md:text-base">
-            Planned Work Orders{" "}
-            <sup className="text-xs font-normal">(Last 30 days)</sup>
-          </p>
-        }
-        extra={
-          <Link
-            href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
-            className="cursor-pointer text-secondary text-xs md:text-sm"
-          >
-            View All
-          </Link>
-        }
-      >
-        <div className="flex justify-center">
-          <DonutChart />
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <p className="text-sm md:text-base">
-            Unplanned Work Orders{" "}
-            <sup className="text-xs font-normal">(Last 3 months)</sup>
-          </p>
-        }
-      >
-        <div className="flex justify-center">
-          <ColumnChart />
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <p className="text-sm md:text-base">
-            Planned Work Orders{" "}
-            <sup className="text-xs font-normal">(Last 3 months)</sup>
-          </p>
-        }
-      >
-        <div className="flex justify-center">
-          <ColumnChart />
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={<p className="text-sm md:text-base">Projected Man Hours</p>}
-        extra={
-          <Link
-            href={`/admin/dashboard?tab=schedule&location=${activeLocation}`}
-            className="cursor-pointer text-secondary text-xs md:text-sm"
-          >
-            View Schdeule
-          </Link>
-        }
-      >
-        <p>Card content</p>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={<p className="text-sm md:text-base">Asset Downtime</p>}
-      >
-        <p>Card content</p>
-      </Card>
+    <div className="flex flex-col gap-6 h-[calc(100dvh-140px)] overflow-auto px-3 lg:px-8 pb-4 pt-3">
+      <div className="grid gap-6">
+        <Card
+          loading={false}
+          className="!bg-primary"
+          title={
+            <p className="text-sm md:text-base">
+              Unplanned Work Orders{" "}
+              <sup className="text-xs font-normal">(Last 30 days)</sup>
+            </p>
+          }
+          extra={
+            <Link
+              href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
+              className="cursor-pointer text-secondary text-xs md:text-sm"
+            >
+              View All
+            </Link>
+          }
+        >
+          <div className="grid md:grid-cols-2">
+            <div className="flex justify-center">
+              <DonutChart />
+            </div>
+          </div>
+        </Card>
+        <Card
+          loading={false}
+          className="!bg-primary"
+          title={
+            <p className="text-sm md:text-base">
+              Planned Work Orders{" "}
+              <sup className="text-xs font-normal">(Last 30 days)</sup>
+            </p>
+          }
+          extra={
+            <Link
+              href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
+              className="cursor-pointer text-secondary text-xs md:text-sm"
+            >
+              View All
+            </Link>
+          }
+        >
+          <div className="grid md:grid-cols-2">
+            <div className="flex justify-center">
+              <DonutChart />
+            </div>
+          </div>
+        </Card>
+      </div>
+      <div className="grid xl:grid-cols-3 gap-6">
+        <Card
+          loading={false}
+          className="!bg-primary"
+          title={
+            <p className="text-sm md:text-base">
+              Unplanned Work Orders{" "}
+              <sup className="text-xs font-normal">(Last 3 months)</sup>
+            </p>
+          }
+        >
+          <div className="flex justify-center">
+            <ColumnChart />
+          </div>
+        </Card>
+        <Card
+          loading={false}
+          className="!bg-primary"
+          title={
+            <p className="text-sm md:text-base">
+              Planned Work Orders{" "}
+              <sup className="text-xs font-normal">(Last 3 months)</sup>
+            </p>
+          }
+        >
+          <div className="flex justify-center">
+            <ColumnChart />
+          </div>
+        </Card>
+        <Card
+          loading={false}
+          className="!bg-primary"
+          title={<p className="text-sm md:text-base">Projected Man Hours</p>}
+          extra={
+            <Link
+              href={`/admin/dashboard?tab=schedule&location=${activeLocation}`}
+              className="cursor-pointer text-secondary text-xs md:text-sm"
+            >
+              View Schdeule
+            </Link>
+          }
+        >
+          <div className="flex justify-center">
+            <BarChart />
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
