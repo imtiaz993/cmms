41ac277a8ab369fc94ec0a7992fe39db635bf850
Imtiaz Hussain <imtiazh993@gmail.com>
2024-10-06 15:25:52 +0500

    added material transfer

---
+++ app/(pages)/admin/dashboard/components/material-transfer/components/actionBar.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { Checkbox, Dropdown, Input, Menu, message } from "antd";
+import {
+  ExportOutlined,
+  FilterOutlined,
+  PlusOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import Button from "@/components/common/Button";
+import MaterialTransferFilter from "./filtersDropdown";
+
+const ActionBar = ({
+  showAddMaterialTransferModal,
+  columns,
+  checkedList,
+  setCheckedList,
+}) => {
+  const [searchText, setSearchText] = useState("");
+  const [showHierarchy, setShowHierarchy] = useState(false);
+
+  const options = columns.map(({ key, title }, index) => ({
+    label: title,
+    value: key,
+    key: index,
+  }));
+
+  const handleCheckboxChange = (value) => {
+    const newCheckedList = checkedList.includes(value)
+      ? checkedList.filter((key) => key !== value)
+      : [...checkedList, value];
+    setCheckedList(newCheckedList);
+  };
+
+  return (
+    <div className="md:flex items-center gap-3 mb-3">
+      <Input
+        placeholder="Search..."
+        onChange={(e) => setSearchText(e.target.value)}
+        style={{ height: "36px" }}
+      />
+      <div className="grid grid-cols-2 sm:flex items-center gap-2 mt-4 md:mt-0">
+        <Dropdown
+          dropdownRender={() => <MaterialTransferFilter />}
+          trigger={["click"]}
+          arrow
+          placement="bottomCenter"
+        >
+          <Button
+            text="Filter"
+            outlined
+            style={{ padding: "4px 0px" }}
+            prefix={<FilterOutlined />}
+          />
+        </Dropdown>
+        <Dropdown
+          dropdownRender={() => (
+            <Menu style={{ background: "#4C4C4C" }}>
+              <Menu.ItemGroup title="Select Columns">
+                {options.map((option) => (
+                  <Menu.Item
+                    key={option.value}
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <Checkbox
+                      value={option.value}
+                      checked={checkedList.includes(option.value)}
+                      onChange={() => {
+                        handleCheckboxChange(option.value);
+                      }}
+                    >
+                      {option.label}
+                    </Checkbox>
+                  </Menu.Item>
+                ))}
+              </Menu.ItemGroup>
+            </Menu>
+          )}
+          trigger={["click"]}
+          arrow
+          placement="bottomCenter"
+        >
+          <Button
+            text="Column Settings"
+            outlined
+            style={{ padding: "4px 24px" }}
+            prefix={<SettingOutlined />}
+          />
+        </Dropdown>
+
+        <Button
+          text="Export"
+          outlined
+          style={{ padding: "4px 0px" }}
+          prefix={<ExportOutlined />}
+          onClick={() => {
+            message.success(
+              "Export initiated with hierarchy: " + showHierarchy
+            );
+          }}
+        />
+        <Button
+          text="New Material Transfer"
+          onClick={showAddMaterialTransferModal}
+          outlined
+          style={{ padding: "4px 35px" }}
+          prefix={<PlusOutlined />}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ActionBar;
--- app/(pages)/admin/dashboard/components/tabs.jsx
+++ app/(pages)/admin/dashboard/components/tabs.jsx
@@ -14,6 +14,7 @@ import Schedule from "./schedule";
 import Inventory from "./inventory";
 import WorkOrders from "./work-orders";
 import Documents from "./documents";
+import MaterialTransfer from "./material-transfer";
 
 const Tabs = () => {
   const router = useRouter();
@@ -74,7 +75,7 @@ const Tabs = () => {
     {
       key: "material-transfer",
       label: "Material Transfer",
-      children: "Content of Material Transfer",
+      children: <MaterialTransfer />,
     },
   ];
   return (
