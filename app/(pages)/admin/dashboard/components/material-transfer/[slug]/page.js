3bb2b09902dd5ee2f1027544fa93dd14fb0937c3
i-akbar <67053767+i-akbar@users.noreply.github.com>
2024-10-09 18:54:58 +0500

    Material Transfer detail complete

--- app/(pages)/admin/dashboard/components/material-transfer/[slug]/page.js
+++ app/(pages)/admin/dashboard/components/material-transfer/[slug]/page.js
@@ -1,11 +1,26 @@
 "use client";
 import Button from "@/components/common/Button";
 import {
+  ArrowLeftOutlined,
+  BackwardFilled,
   ExclamationCircleOutlined,
   ExportOutlined,
   FolderFilled,
+  MailOutlined,
+  PrinterFilled,
 } from "@ant-design/icons";
-import { Badge, Card, Dropdown, Menu, Table, Tabs } from "antd";
+import {
+  BackTop,
+  Badge,
+  Card,
+  Dropdown,
+  Menu,
+  message,
+  Steps,
+  Table,
+  Tabs,
+  Typography,
+} from "antd";
 import ViewAssetsDetailsPopup from "./viewAssetsDetailsPopup";
 import { useState } from "react";
 import UploadLinkDocPopup from "./uploadLinkDocPopup";
@@ -76,6 +91,8 @@ const MaterialTransferDetail = () => {
   const [assetDetailsPopup, setAssetDetailsPopup] = useState(false);
   const [uploadLinkDocVisible, setUploadLinkDocVisible] = useState(false);
   const [uploadDocVisible, setUploadDocVisible] = useState(false);
+  const { Step } = Steps;
+  const { Text } = Typography;
 
   const tabs = [
     {
@@ -103,13 +120,15 @@ const MaterialTransferDetail = () => {
               onClick={() => {}}
               fullWidth={false}
               text="Add Assets"
+              disabled
             />
             <Button
               type="primary"
               // style={{ height: "36px", fontWeight: "500", minWidth: "80px" }}
-              onClick={() => {}}
+
               fullWidth={false}
               text="Export"
+              onClick={() => message.info("Export will be available soon.")}
               prefix={<ExportOutlined />}
             />
           </div>
@@ -163,173 +182,261 @@ const MaterialTransferDetail = () => {
         visible={uploadDocVisible}
         setVisible={setUploadDocVisible}
       />
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <div className="flex items-center gap-2">
-            <span className="px-2 py-1 bg-secondary rounded-full">
-              <FolderFilled />
-            </span>
-            MT14687000001 <p className="text-xs font-normal">(Approved)</p>
-          </div>
-        }
-
-        // extra={
-        //   <Link
-        //     href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
-        //     className="cursor-pointer text-secondary text-xs md:text-sm"
-        //   >
-        //     View All
-        //   </Link>
-        // }
-      >
-        <div className="grid md:grid-cols-2 mx-2 gap-3">
-          <div>
-            <span className="opacity-70 mr-3">Origin</span>
-            <span className=""> Rig 23 - Electrical Systems</span>
-          </div>
-          <div>
-            <span className="opacity-70 mr-3">Transporter</span>
-            <span className=""> Rig 23</span>
-          </div>
-          <div>
-            <span className="opacity-70 mr-3">Destination</span>
-            <span className=""> Rig 21 - Electrical Systems</span>
-          </div>
-          <div>
-            <span className="opacity-70 mr-3">Attention To</span>
-            <span className="">Aaron Cannon</span>
-          </div>
-          {/* <Table
-            loading={false}
-            size={"small"}
-            columns={unplannedColumns}
-            dataSource={unplannedData}
-            pagination={false}
-            style={{
-              overflow: "auto",
-            }}
-          /> */}
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={<div>Transfer Details</div>}
-        style={{ marginTop: "20px" }}
-      >
-        <div className="grid md:grid-cols-3 mx-2 gap-3">
-          <div>
-            <span className="opacity-70 mr-3">Creator</span>
-            <span className="">Manager, Rig 23</span>
-          </div>
-          <div>
-            <span className="opacity-70 mr-3">Created On</span>
-            <span className="">Feb 12, 2024</span>
-          </div>
-          <div className="flex">
-            <span className="opacity-70 mr-3 block">
-              Material Transfer Type
-            </span>
-            <span className="">NORAM Yard to Rig</span>
-          </div>
-          <div>
-            <span className="opacity-70 mr-3">Cautions</span>
-            <span className="">Non-Hazardous</span>
-          </div>
-          <div className="md:col-span-2">
-            <span className="opacity-70 mr-3">Comments</span>
-            <span className="">Ship 444t 125hp rig house motor to rig 21</span>
-          </div>
-
-          {/* <Table
-            loading={false}
-            size={"small"}
-            columns={unplannedColumns}
-            dataSource={unplannedData}
-            pagination={false}
-            style={{
-              overflow: "auto",
-            }}
-          /> */}
-        </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={null}
-        style={{ marginTop: "20px" }}
-      >
+      <div className="flex justify-between gap-3 mb-5">
+        <Button
+          prefix={<ArrowLeftOutlined />}
+          onClick={() => router.back()}
+          fullWidth={false}
+          outlined
+        />
         <div className="">
-          <Tabs
-            type="card"
-            size={"small"}
-            items={tabs.map((i, index) => ({ ...i, key: index }))}
-            tabBarStyle={{ borderColor: "white" }}
-            className="reports-tabs"
+          <Button
+            text="Email"
+            prefix={<MailOutlined />}
+            fullWidth={false}
+            onClick={() => message.info("Email will be available soon.")}
+          />
+
+          <Button
+            text="Print"
+            prefix={<PrinterFilled />}
+            fullWidth={false}
+            className="ml-3"
+            onClick={() => message.info("Print will be available soon.")}
           />
         </div>
-      </Card>
-      <Card
-        loading={false}
-        className="!bg-primary"
-        title={
-          <div className="flex items-center justify-between gap-3">
-            <Badge count={0}>
-              <h1 className="text-base">Material Transfer Documents</h1>
-            </Badge>
-            <div>
+      </div>
+      <div className="flex gap-5">
+        <div className="w-3/5">
+          <Card
+            loading={false}
+            className="!bg-primary"
+            title={
+              <div className="flex items-center gap-2">
+                <span className="px-2 py-1 bg-secondary rounded-full">
+                  <FolderFilled />
+                </span>
+                MT14687000001 <p className="text-xs font-normal">(Approved)</p>
+              </div>
+            }
+
+            // extra={
+            //   <Link
+            //     href={`/admin/dashboard?tab=work-orders&location=${activeLocation}`}
+            //     className="cursor-pointer text-secondary text-xs md:text-sm"
+            //   >
+            //     View All
+            //   </Link>
+            // }
+          >
+            <div className="grid md:grid-cols-2 mx-2 gap-3">
+              <div>
+                <span className="opacity-70 mr-3">Origin</span>
+                <span className=""> Rig 23 - Electrical Systems</span>
+              </div>
+              <div>
+                <span className="opacity-70 mr-3">Transporter</span>
+                <span className=""> Rig 23</span>
+              </div>
+              <div>
+                <span className="opacity-70 mr-3">Destination</span>
+                <span className=""> Rig 21 - Electrical Systems</span>
+              </div>
+              <div>
+                <span className="opacity-70 mr-3">Attention To</span>
+                <span className="">Aaron Cannon</span>
+              </div>
+              {/* <Table
+            loading={false}
+            size={"small"}
+            columns={unplannedColumns}
+            dataSource={unplannedData}
+            pagination={false}
+            style={{
+              overflow: "auto",
+            }}
+          /> */}
+            </div>
+          </Card>
+          <Card
+            loading={false}
+            className="!bg-primary"
+            title={<div>Transfer Details</div>}
+            style={{ marginTop: "20px" }}
+          >
+            <div className="grid md:grid-cols-3 mx-2 gap-3">
+              <div>
+                <span className="opacity-70 mr-3">Creator</span>
+                <span className="">Manager, Rig 23</span>
+              </div>
+              <div>
+                <span className="opacity-70 mr-3">Created On</span>
+                <span className="">Feb 12, 2024</span>
+              </div>
+              <div className="flex">
+                <span className="opacity-70 mr-3 block">
+                  Material Transfer Type
+                </span>
+                <span className="">NORAM Yard to Rig</span>
+              </div>
+              <div>
+                <span className="opacity-70 mr-3">Cautions</span>
+                <span className="">Non-Hazardous</span>
+              </div>
+              <div className="md:col-span-2">
+                <span className="opacity-70 mr-3">Comments</span>
+                <span className="">
+                  Ship 444t 125hp rig house motor to rig 21
+                </span>
+              </div>
+
+              {/* <Table
+            loading={false}
+            size={"small"}
+            columns={unplannedColumns}
+            dataSource={unplannedData}
+            pagination={false}
+            style={{
+              overflow: "auto",
+            }}
+          /> */}
+            </div>
+          </Card>
+          <Card
+            loading={false}
+            className="!bg-primary"
+            title={null}
+            style={{ marginTop: "20px" }}
+          >
+            <div className="">
+              <Tabs
+                type="card"
+                size={"small"}
+                items={tabs.map((i, index) => ({ ...i, key: index }))}
+                tabBarStyle={{ borderColor: "white" }}
+                className="reports-tabs"
+              />
+            </div>
+          </Card>
+        </div>
+        <div className="w-2/5">
+          <Card
+            loading={false}
+            title={<p>Workflow Status</p>}
+            extra={
               <Button
                 outlined
                 size="small"
-                text="View Details"
                 fullWidth={false}
                 className="!text-xs !h-7 mr-3"
-                disabled
+                text="Export"
+                prefix={<ExportOutlined />}
+                onClick={() => message.info("Export will be available soon.")}
               />
-              <Dropdown
-                dropdownRender={() => (
-                  <Menu style={{ background: "#4C4C4C" }}>
-                    <Menu.ItemGroup title={null}>
-                      <Menu.Item
-                        key={0}
-                        style={{ display: "flex", alignItems: "center" }}
-                        onClick={() => setUploadDocVisible(true)}
-                      >
-                        Upload Document
-                      </Menu.Item>
-                      <Menu.Item
-                        key={1}
-                        style={{ display: "flex", alignItems: "center" }}
-                        onClick={() => setUploadLinkDocVisible(true)}
-                      >
-                        Link Document
-                      </Menu.Item>
-                    </Menu.ItemGroup>
-                  </Menu>
-                )}
-                trigger={["click"]}
-                arrow
-                // placement="bottomCenter"
-              >
-                <Button
-                  outlined
-                  size="small"
-                  text="Upload"
-                  fullWidth={false}
-                  className="!text-xs !h-7"
-                />
-              </Dropdown>
+            }
+          >
+            <Steps
+              direction="vertical"
+              current={1} // Adjust this based on the progress
+              progressDot
+            >
+              <Step
+                title={<p className="text-sm">Ship (NORAM Yard to Rig)</p>}
+                description={
+                  <div>
+                    <p>
+                      <span className="text-[#52c41a]">Approved</span> by Rig 23
+                      Manager on 02/12/2024 19:58 (GMT+5)
+                    </p>
+                    <span className="opacity-50">Comment:</span>
+                    <span className="font-italic"> "Ship to rig 21"</span>
+                    <p>
+                      <span className="opacity-50">Primary Approver:</span>{" "}
+                      <strong>Zeba Avera</strong>
+                    </p>
+                  </div>
+                }
+              />
+              <Step
+                title={<p className="text-sm">Receive (NORAM Yard to Rig)</p>}
+                description={
+                  <div>
+                    <p>
+                      <span className="text-[#52c41a]">Approved</span> by
+                      Midland Yard on 02/23/2024 18:42 (GMT+5)
+                    </p>
+                    <span className="opacity-50"> Comment:</span>{" "}
+                    <span className="font-italic">"AT RIG21"</span>
+                    <p>
+                      <span className="opacity-50">Primary Approver:</span>{" "}
+                      <strong>Zeba Avera</strong>
+                    </p>
+                  </div>
+                }
+              />
+            </Steps>
+          </Card>
+          <Card
+            loading={false}
+            className="!bg-primary"
+            title={
+              <div className="flex items-center justify-between gap-3">
+                <Badge count={0}>
+                  <h1 className="text-base">Material Transfer Documents</h1>
+                </Badge>
+                <div>
+                  <Button
+                    outlined
+                    size="small"
+                    text="View Details"
+                    fullWidth={false}
+                    className="!text-xs !h-7 mr-3"
+                    disabled
+                  />
+                  <Dropdown
+                    dropdownRender={() => (
+                      <Menu style={{ background: "#4C4C4C" }}>
+                        <Menu.ItemGroup title={null}>
+                          <Menu.Item
+                            key={0}
+                            style={{ display: "flex", alignItems: "center" }}
+                            onClick={() => setUploadDocVisible(true)}
+                          >
+                            Upload Document
+                          </Menu.Item>
+                          <Menu.Item
+                            key={1}
+                            style={{ display: "flex", alignItems: "center" }}
+                            onClick={() => setUploadLinkDocVisible(true)}
+                          >
+                            Link Document
+                          </Menu.Item>
+                        </Menu.ItemGroup>
+                      </Menu>
+                    )}
+                    trigger={["click"]}
+                    arrow
+                    // placement="bottomCenter"
+                  >
+                    <Button
+                      outlined
+                      size="small"
+                      text="Upload"
+                      fullWidth={false}
+                      className="!text-xs !h-7"
+                    />
+                  </Dropdown>
+                </div>
+              </div>
+            }
+            style={{ marginTop: "20px" }}
+          >
+            <div className="text-center my-7">
+              <ExclamationCircleOutlined /> No data to display
             </div>
-          </div>
-        }
-        style={{ marginTop: "20px" }}
-      >
-        <div className="text-center my-7">
-          <ExclamationCircleOutlined /> No data to display
+          </Card>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
