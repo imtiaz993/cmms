bd9875f797716fd6829a95c347679df15cf557b1
i-akbar <67053767+i-akbar@users.noreply.github.com>
2024-10-28 17:38:17 +0500

    Adding Inventory changes

--- app/(pages)/admin/dashboard/components/inventory/components/createInventoryPopup.jsx
+++ app/(pages)/admin/dashboard/components/inventory/components/createInventoryPopup.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import { message, Modal, Select, Steps } from "antd";
+import { DatePicker, message, Modal, Select, Steps } from "antd";
 import InputField from "@/components/common/InputField";
 import Button from "@/components/common/Button";
+import TextArea from "antd/es/input/TextArea";
 
 const validationSchema = Yup.object().shape({
   costCenter: Yup.string(),
 });
 
-const CreateInventoryPopup = ({ addInventoryVisible, setAddInventoryVisible }) => {
+const CreateInventoryPopup = ({
+  addInventoryVisible,
+  setAddInventoryVisible,
+}) => {
   const [currentStep, setCurrentStep] = useState(0);
 
   const handleNext = () => {
@@ -32,21 +36,6 @@ const CreateInventoryPopup = ({ addInventoryVisible, setAddInventoryVisible }) =
     // }
   };
 
-  const steps = [
-    {
-      title: "Summary",
-    },
-    {
-      title: "Details",
-    },
-    {
-      title: "Maintenance",
-    },
-    {
-      title: "Maintenance Status",
-    },
-  ];
-
   return (
     <Formik
       initialValues={{
@@ -63,42 +52,31 @@ const CreateInventoryPopup = ({ addInventoryVisible, setAddInventoryVisible }) =
         <Form onSubmit={handleSubmit}>
           <Modal
             maskClosable={false}
-            title={<h1 className="text-lg md:text-2xl mb-5">Add New Inventory</h1>}
+            title={
+              <h1 className="text-lg md:text-2xl mb-5">Add New Inventory</h1>
+            }
             open={addInventoryVisible}
             onCancel={() => setAddInventoryVisible(false)}
             footer={
               <div>
-                {currentStep > 0 && (
-                  <Button
-                    className="mr-2"
-                    onClick={handlePrev}
-                    outlined
-                    size="small"
-                    text="Previous"
-                    fullWidth={false}
-                    disabled={isSubmitting}
-                  />
-                )}
-                {currentStep < steps.length - 1 && (
-                  <Button
-                    className="mr-2"
-                    onClick={handleNext}
-                    size="small"
-                    text="Next"
-                    fullWidth={false}
-                  />
-                )}
-                {currentStep === steps.length - 1 && (
-                  <Button
-                    className="mr-2"
-                    htmlType="submit"
-                    isLoading={isSubmitting}
-                    disabled={isSubmitting}
-                    size="small"
-                    text="Add Inventory"
-                    fullWidth={false}
-                  />
-                )}
+                <Button
+                  className="mr-2"
+                  onClick={handlePrev}
+                  outlined
+                  size="small"
+                  text="Cancel"
+                  fullWidth={false}
+                  disabled={isSubmitting}
+                />
+                <Button
+                  className="mr-2"
+                  htmlType="submit"
+                  isLoading={isSubmitting}
+                  disabled={isSubmitting}
+                  size="small"
+                  text="Add Inventory"
+                  fullWidth={false}
+                />
               </div>
             }
             width={1000}
@@ -108,125 +86,95 @@ const CreateInventoryPopup = ({ addInventoryVisible, setAddInventoryVisible }) =
               overflowX: "hidden",
             }}
           >
-            <div className="mb-5 md:mb-10">
-              <Steps current={currentStep} items={steps} />
-            </div>
-
             <div>
-              {currentStep === 0 && (
-                <div className="grid md:grid-cols-3 gap-4">
-                  <InputField
-                    name="costCenter"
-                    placeholder="Cost Center"
-                    maxLength={128}
+              <div className="grid md:grid-cols-3 gap-4">
+                <InputField
+                  name="physicalLocation"
+                  placeholder="Physical Location"
+                  maxLength={128}
+                />
+                <InputField
+                  name="makeModel"
+                  placeholder="Make and/or Model"
+                  maxLength={128}
+                />
+                <InputField
+                  name="partItem"
+                  placeholder="Part # / Item #"
+                  maxLength={128}
+                />
+                <InputField
+                  name="assetNumber"
+                  placeholder="Asset # / Serial #"
+                  maxLength={128}
+                />
+                <InputField
+                  name="quantity"
+                  placeholder="Quantity"
+                  maxLength={50}
+                />
+                <InputField
+                  name="category"
+                  placeholder="Type / Category"
+                  maxLength={128}
+                />
+                <div className="md:col-span-3">
+                  <Field
+                    as={TextArea}
+                    name="description"
+                    placeholder="Description"
+                    maxLength={150}
                   />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Parent Inventory"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Accounting Dept."
-                  />
-                  <InputField name="costCenter" placeholder="Subunit" />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Inventory Class"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Alternative ID #"
-                    maxLength={128}
-                  />
-                </div>
-              )}
+                  <div className="text-right">0/150</div>
 
-              {currentStep === 1 && (
-                <div className="grid md:grid-cols-3 gap-4">
-                  <InputField
-                    name="costCenter"
-                    placeholder="Physical Location"
-                    maxLength={128}
+                  <Field
+                    as={TextArea}
+                    name="specDetails"
+                    placeholder="Spec Details"
+                    maxLength={500}
                   />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Inventory #"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="RFID/Barcode"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Installed Date"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Serial #"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="OEM Serial #"
-                    maxLength={50}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Estimated Life (months)"
-                    maxLength={128}
-                  />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Downtime Cost Per Hour"
-                    maxLength={128}
-                  />
-                  <Select placeholder="Unit" />
-                  <div className="md:col-span-3">
-                    <InputField
-                      name="costCenter"
-                      placeholder="Description"
-                      maxLength={150}
+                  <div className="text-right">0/500</div>
+                  <div className="grid md:grid-cols-3 gap-4">
+                    <Field
+                      as={DatePicker}
+                      placeholder="Received Date"
+                      maxLength={10}
+                      name="recievedDate"
+                      style={{ height: "36px" }}
                     />
-                    <div className="text-right">0/150</div>
-                  </div>
-                  <div className="md:col-span-3">
-                    <InputField
-                      name="costCenter"
-                      placeholder="Spec Details"
-                      maxLength={500}
+                    <Select
+                      name="supplier"
+                      placeholder="Suppliers"
+                      style={{ height: "36px" }}
                     />
-                    <div className="text-right">0/500</div>
+                    <Field
+                      as={DatePicker}
+                      name="installedDate"
+                      placeholder="Installed Date"
+                      style={{ height: "36px" }}
+                    />
+                    <Field
+                      as={DatePicker}
+                      name="originalManufactureDate"
+                      placeholder="Original Manufacture Date"
+                      style={{ height: "36px" }}
+                    />
+                    <Select
+                      name="condition"
+                      placeholder="Condition"
+                      options={[
+                        { label: "Good", value: "good" },
+                        { label: "Bad", value: "bad" },
+                        { label: "Damaged", value: "damaged" },
+                        { label: "New", value: "new" },
+                        { label: "Rebuilt", value: "rebuilt" },
+                        { label: "Re-Certified", value: "re-certified" },
+                      ]}
+                      style={{ height: "36px" }}
+                    />
                   </div>
                 </div>
-              )}
-
-              {currentStep === 2 && (
-                <div className="grid md:grid-cols-3 gap-4">
-                  <Select placeholder="Criticality" />
-                  <InputField
-                    placeholder="Original Mfr. Date (MM/DD/YYYY)"
-                    maxLength={10}
-                    name="costCenter"
-                  />
-                  <Select placeholder="Condition" />
-                </div>
-              )}
-
-              {currentStep === 3 && (
-                <div className="grid md:grid-cols-3 gap-4">
-                  <Select placeholder="Maint. Status" />
-                  <InputField
-                    name="costCenter"
-                    placeholder="Maint. Start Date (MM/DD/YYYY)"
-                    maxLength={10}
-                  />
-                </div>
-              )}
+              </div>
             </div>
           </Modal>
         </Form>
