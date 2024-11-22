f4e9c381c269d3fe4c74fb98d2086b0fa6d93d2f
i-akbar <67053767+i-akbar@users.noreply.github.com>
2024-11-22 22:20:41 +0500

    Add material Transfer API

--- app/services/materialTransfer/index.js
+++ app/services/materialTransfer/index.js
@@ -13,6 +13,21 @@ export const getMaterialTransferData = async () => {
   }
 };
 
+export const addMaterialTransfer = async (values) => {
+  try {
+    const { status, data } = await authRequest({
+      url: "/material-transfer/add",
+      method: "POST",
+      data: values,
+    });
+    return { status, data };
+  } catch (e) {
+    if (e.data) {
+      return { status: e.status, data: e.data };
+    }
+  }
+};
+
 export const getFilteredMT = async (values) => {
   try {
     const { status, data } = await authRequest({
