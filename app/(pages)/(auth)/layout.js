78e3bab2f6b8daf92445243339cb5386caf2bfcf
i-akbar <67053767+i-akbar@users.noreply.github.com>
2025-01-15 20:08:20 +0500

    Auth Layout Updated

--- app/(pages)/(auth)/layout.js
+++ app/(pages)/(auth)/layout.js
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { getUser } from "@/utils";
+import RightAuthImage from "@/components/rightAuthImage";
+import AuthNavbar from "./authNavbar";
 
 export default function Layout({ children }) {
   const router = useRouter();
@@ -23,7 +25,26 @@ export default function Layout({ children }) {
     <>
       {(!user || !user.role) && (
         <>
-          <div>{children}</div>
+          <AuthNavbar />
+          {/* Background Layers */}
+          <div className="absolute inset-0">
+            {/* Triangle Background */}
+            <div
+              className="w-full mt-56 h-[200px] bg-[#3F3F3F]"
+              style={{ clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)" }}
+            ></div>
+
+            {/* Black Background */}
+            <div className="bg-[#3F3F3F]  h-[calc(100vh-224px-200px)]"></div>
+          </div>
+
+          {/* Content Section */}
+          <div className="relative md:flex min-h-screen w-11/12 mx-auto md:w-full">
+            <div className="md:w-7/12 md:mx-10">{children}</div>
+            <div className="w-5/12 pt-40 hidden md:block">
+              <RightAuthImage />
+            </div>
+          </div>
         </>
       )}
     </>
---
+++ components/rightAuthImage.jsx
@@ -0,0 +1,11 @@
+const RightAuthImage = () => {
+  return (
+    <div className="bg-primary p-4 h-full">
+      <h1 className="text-3xl font-bold text-center flex justify-center items-center h-full">
+        Image Of Platform
+      </h1>
+    </div>
+  );
+};
+
+export default RightAuthImage;
