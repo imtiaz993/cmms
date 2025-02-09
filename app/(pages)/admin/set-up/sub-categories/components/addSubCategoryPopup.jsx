f3eef911099d78536da01f6a1493919ca66aa52d
i-akbar <67053767+i-akbar@users.noreply.github.com>
2025-02-10 01:20:39 +0500

    Add Sub Category

---
+++ app/(pages)/admin/set-up/sub-categories/components/addSubCategoryPopup.jsx
@@ -0,0 +1,69 @@
+import Button from "@/components/common/Button";
+import InputField from "@/components/common/InputField";
+import SelectField from "@/components/common/SelectField";
+import { Form, Modal } from "antd";
+import { Formik } from "formik";
+
+const AddSubCategoryPopup = ({ visible, setVisible }) => {
+  const handleSubmit = () => {
+    console.log("Submitted");
+  };
+  return (
+    <div className="">
+      <Formik initialValues={{}} onSubmit={handleSubmit}>
+        {({ isSubmitting, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <Modal
+              maskClosable={false}
+              title={
+                <h1 className="text-lg md:text-2xl mb-5">Add a Sub-Category</h1>
+              }
+              open={visible}
+              onCancel={() => setVisible(false)}
+              footer={
+                <div>
+                  <Button
+                    className="mr-4"
+                    onClick={() => setVisible(false)}
+                    outlined
+                    size="small"
+                    text="Cancel"
+                    fullWidth={false}
+                    disabled={false}
+                  />
+
+                  <Button
+                    className=""
+                    onClick={handleSubmit}
+                    size="small"
+                    text="Add"
+                    fullWidth={false}
+                    disabled={false}
+                  />
+                </div>
+              }
+              width={600}
+            >
+              <SelectField
+                name="category"
+                placeholder="Select category"
+                label="Category"
+                required
+              />
+              <div className="mt-5">
+                <InputField
+                  name="subCategory"
+                  placeholder="Enter sub-category name"
+                  label="Sub-Category"
+                  required
+                />
+              </div>
+            </Modal>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default AddSubCategoryPopup;
