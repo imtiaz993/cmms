3c258c112151b4c37c5341d0c75d4f510c8c57d5
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-04-02 20:57:15 +0500

    added video

---
+++ app/(pages)/testing/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const Page = () => {
+  return (
+    <iframe
+      src="https://yodo1net-my.sharepoint.com/personal/zhaoyajuan_yodo1_com/_layouts/15/embed.aspx?UniqueId=b91bd3aa-ee5f-4b75-bdb0-600f9d492f0e&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create"
+      width="1280"
+      height="720"
+      frameBorder="0"
+      scrolling="no"
+      allowFullScreen
+      title="Meta Channel-20250402_051632-Meeting Recording.mp4"
+    ></iframe>
+  );
+};
+
+export default Page;
